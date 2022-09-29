90beb69bd4d8bc980cbc7ae784398a080f551a8e
battler82004 <34427808+battler82004@users.noreply.github.com>
2022-09-29 10:57:09 -0400

    Update recursiveAsteriskGen2.cpp

--- miscSingleFile/recursiveAsteriskGen2.cpp
+++ miscSingleFile/recursiveAsteriskGen2.cpp
@@ -11,51 +11,53 @@ using namespace std;
 
 // Function prototype
 void asteriskGenerator(int);
-  // This function takes in the total number of asterisks in the first
-  // line and then recursively calls itself to ouput the correct num
-  // of asterisks
+    // This function takes in the total number of asterisks in the first
+    // line and then calls 2 recursive functions to ouput the correct num
+    // of asterisks
 void deAsterisks(int);
+    // This funciton outputs the asterisks when the num is decreasing
 void inAsterisks(int, int = 1);
+    // This function outputs the asterisks when the num is increasing
 
 int main() {
-  // Variable declaration
-  int numOfLines;
+    // Variable declaration
+    int numOfLines;
 
-  // User input
-  cout << "Enter the number of lines: ";
-  cin >> numOfLines;
+    // User input
+    cout << "Enter the number of lines: ";
+    cin >> numOfLines;
 
-  asteriskGenerator(numOfLines); // Output
+    asteriskGenerator(numOfLines); // Output
 
-  return 0;
+    return 0;
 }
 
 // This function takes in the total number of asterisks in the first
-// line and then recursively calls itself to ouput the correct num
+// line and then calls 2 recursive functions to ouput the correct num
 // of asterisks
 void asteriskGenerator(int numOfAsterisks) {
     deAsterisks(numOfAsterisks);
     inAsterisks(numOfAsterisks);
 }
 
+// This funciton outputs the asterisks when the num is decreasing
 void deAsterisks(int num) {
-    // Output curr line
-    for (int i = 0; i < num; i++) {
+    for (int i = 0; i < num; i++) { // Outputs curr line
         cout << '*';
     }
     cout << endl;
     
-    if (num != 1)
+    if (num != 1) // If general case
         deAsterisks(num-1);
 }
 
+// This funciton outputs the asterisks when the num is increasing
 void inAsterisks(int max, int curr) {
-    // Output curr line
-    for (int i = 0; i < curr; i++) {
+    for (int i = 0; i < curr; i++) { // Outputs curr line
         cout << '*';
     }
     cout << endl;
     
-    if (curr != max)
+    if (curr != max) // If general case
         inAsterisks(max, curr+1);
 }
