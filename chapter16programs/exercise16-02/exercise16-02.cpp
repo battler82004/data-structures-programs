3489cbe816164b218f0e7ca6ecc3927763e4be27
battler82004 <34427808+battler82004@users.noreply.github.com>
2022-10-13 11:05:27 -0400

    Create exercise16-02.cpp

---
+++ chapter16programs/exercise16-02/exercise16-02.cpp
@@ -0,0 +1,31 @@
+/*
+  Title: Exercise 16-2 (deleteSmallest and removeAll Linked List)
+  Description: This program adds and tests the functions deleteSmallest
+    and removeAll to the 'unorderedLinkedList' class.
+  Author: James Taddei
+  Date: 10/6/21
+*/
+
+#include <iostream>
+#include "linkedList.h"
+#include "unorderedLinkedList.h"
+using namespace std;
+
+int main() {
+  unorderedLinkedList<int> testList;
+  testList.insertLast(13);
+  testList.insertLast(14);
+  testList.insertLast(17);
+  testList.insertLast(14);
+  testList.insertLast(33);
+
+  testList.print();
+  testList.deleteSmallest();
+  testList.print();
+  testList.removeAll(14);
+  testList.print();
+
+  cout << "Test" << endl;
+
+  return 0;
+}
